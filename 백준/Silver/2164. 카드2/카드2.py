c5ccf8fc5ddbe6ea6f0041a03b68208d5015dbc1
hosunny <109336653+hosunny@users.noreply.github.com>
2022-08-21 00:46:57 +0900

    [Silver IV] Title: 카드2, Time: 304 ms, Memory: 49320 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+N = int(input())
+lst = deque()
+for i in range(N, 0, -1):
+    lst.append(i)
+while len(lst) > 1:
+    lst.pop()
+    lst.insert(0, lst[-1])
+    lst.pop()
+print(lst[0])
