c67c9ca68476a586417df771e5916e4294c3fff6
hosunny <109336653+hosunny@users.noreply.github.com>
2022-08-28 19:09:01 +0900

    [Silver IV] Title: 빙고, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -0,0 +1,37 @@
+bingo = list(list(map(int, input().split())) for _ in range(5))
+turn = list(list(map(int, input().split())) for _ in range(5))
+bb = []
+for i in bingo:
+    for j in i:
+        bb.append(j)
+tt = []
+for i in turn:
+    for j in i:
+        tt.append(j)
+cnt = 0
+for k in tt:
+    i = bb.index(k) // 5
+    j = bb.index(k) % 5
+    bingo[i][j] = 0
+    if i == j:
+        if bingo[i][j] == bingo[(i + 1) % 5][(i + 1) % 5] == bingo[(i + 2) % 5][(i + 2) % 5] == bingo[(i + 3) % 5][(i + 3) % 5] == bingo[(i + 4) % 5][(i + 4) % 5] == 0:
+            cnt += 1
+            if cnt == 3:
+                print(tt.index(k) + 1)
+                break
+    if i + j == 4:
+        if bingo[i][j] == bingo[(i + 1) % 5][4 -((i + 1) % 5)] == bingo[(i + 2) % 5][4 - ((i + 2) % 5)] == bingo[(i + 3) % 5][4 - ((i + 3) % 5)] == bingo[(i + 4) % 5][4 - ((i + 4) % 5)] == 0:
+            cnt += 1
+            if cnt == 3:
+                print(tt.index(k) + 1)
+                break
+    if bingo[i].count(0) == 5:
+        cnt += 1
+        if cnt == 3:
+            print(tt.index(k) + 1)
+            break
+    if bingo[i][j] == bingo[(i + 1) % 5][j] == bingo[(i + 2) % 5][j] == bingo[(i + 3) % 5][j] == bingo[(i + 4) % 5][j] == 0:
+        cnt += 1
+        if cnt == 3:
+            print(tt.index(k) + 1)
+            break
