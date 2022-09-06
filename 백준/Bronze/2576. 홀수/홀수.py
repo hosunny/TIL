736bbb1582deadec1592a6c8c7526463090f6e7e
hosunny <109336653+hosunny@users.noreply.github.com>
2022-09-06 23:59:58 +0900

    [Bronze III] Title: 홀수, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2576. 홀수/홀수.py
@@ -0,0 +1,14 @@
+score = 0
+mm = 100
+for i in range(7):
+    n = int(input())
+    if n % 2 != 0:
+        score += n
+        if n < mm:
+            mm = n
+if score == 0:
+    print(-1)
+else:
+    print(score)
+if score != 0:
+    print(mm)
