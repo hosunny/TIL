f5eeffcf00206b6b1a2b851bb377527b4d16ce8b
hosunny <109336653+hosunny@users.noreply.github.com>
2022-09-22 04:20:28 +0900

    [Silver III] Title: 피보나치 함수, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,9 @@
+T = int(input())
+for H in range(T):
+    N = int(input())
+    memo = [0] * 41
+    memo[0] = [1, 0]
+    memo[1] = [0, 1]
+    for i in range(2, N + 1):
+        memo[i] = [memo[i - 2][0] + memo[i - 1][0], memo[i - 2][1] + memo[i - 1][1]]
+    print(memo[N][0], memo[N][1])
